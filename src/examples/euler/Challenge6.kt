718c10f5ed31729e006b165322ab407303faf63c
Ken Anderson <ken.anderson@urlintegration.com>
2016-06-01 14:17:29 -0600

    Updated the benchmark to use nanoseconds

--- src/examples/euler/Challenge6.kt
+++ src/examples/euler/Challenge6.kt
@@ -1,17 +1,29 @@
 package examples.euler;
 
-import kotlin.system.measureTimeMillis
+import kotlin.system.measureNanoTime;
+
+/**
+ * The sum of the squares of the first ten natural numbers is,
+ * 12 + 22 + ... + 102 = 385
+ * The square of the sum of the first ten natural numbers is,
+ *
+ * (1 + 2 + ... + 10)2 = 552 = 3025
+ * Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+ *
+ * Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+ */
+
 
 const internal val MAXC6 = 100
 
 fun main(args: Array<String>) {
     var c6 = Challenge6();
-    var benchMark = measureTimeMillis {
+    var benchMark = measureNanoTime {
         var sumOfSquares = c6.sumOfSquares(MAXC6);
         var squareOfSum = c6.squareOfSum(MAXC6);
         println(squareOfSum - sumOfSquares);
     }
-    print("Process completed in $benchMark milliseconds")
+    print("Process completed in $benchMark nanoseconds")
 }
 
 class Challenge6 {
